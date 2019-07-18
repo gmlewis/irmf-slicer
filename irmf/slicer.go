670d6e6be2adedbae2a0b3d394e096a2e3502278
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-07-17 22:33:35 -0400

    First run of slicer

--- irmf/slicer.go
+++ irmf/slicer.go
@@ -22,9 +22,9 @@ func init() {
 
 // Slicer represents a slicer context.
 type Slicer struct {
-	irmf    *IRMF
-	window  *glfw.Window
-	microns float64
+	irmf   *IRMF
+	window *glfw.Window
+	delta  float64 // millimeters (model units)
 
 	program      uint32
 	modelUniform int32
@@ -51,7 +51,8 @@ func Init(view bool, width, height int, micronsResolution float64) *Slicer {
 
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	fmt.Println("OpenGL version", version)
-	return &Slicer{window: window, microns: micronsResolution}
+	// TODO: Support units other than millimeters.
+	return &Slicer{window: window, delta: micronsResolution / 1000.0}
 }
 
 // New prepares the slicer to slice a new shader model.
@@ -80,18 +81,24 @@ func (s *Slicer) Slice(zipName string) error {
 		return fmt.Errorf("compile shader: %v", err)
 	}
 
-	img, err := s.renderSlice(0.0)
-	if err != nil {
-		return fmt.Errorf("renderSlice: %v", err)
-	}
-	n := 0
-	filename := fmt.Sprintf("slices/out%04d.png", n)
-	f, err := w.Create(filename)
-	if err != nil {
-		return fmt.Errorf("Unable to create ZIP file %q: %v", filename, err)
-	}
-	if err := png.Encode(f, img); err != nil {
-		return fmt.Errorf("PNG encode: %v", err)
+	var n int
+	for z := s.irmf.Min[2] + s.delta; z <= s.irmf.Max[2]; z += s.delta {
+		img, err := s.renderSlice(0.0)
+		if err != nil {
+			return fmt.Errorf("renderSlice: %v", err)
+		}
+		filename := fmt.Sprintf("slices/out%04d.png", n)
+		f, err := w.Create(filename)
+		if err != nil {
+			return fmt.Errorf("Unable to create ZIP file %q: %v", filename, err)
+		}
+		if err := png.Encode(f, img); err != nil {
+			return fmt.Errorf("PNG encode: %v", err)
+		}
+		n++
+
+		s.window.SwapBuffers()
+		glfw.PollEvents()
 	}
 
 	if err := w.Close(); err != nil {
@@ -121,12 +128,20 @@ func (s *Slicer) renderSlice(z float64) (image.Image, error) {
 
 	gl.DrawArrays(gl.TRIANGLES, 0, 2*3) // 6*2*3)
 
+	width, height := s.window.GetFramebufferSize()
+	rgba := &image.RGBA{
+		Pix:    make([]uint8, width*height*4),
+		Stride: width * 4, // bytes between vertically adjacent pixels.
+		Rect:   image.Rect(0, 0, width, height),
+	}
+	gl.ReadPixels(0, 0, int32(width), int32(height), gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(&rgba.Pix[0]))
+
 	// Maintenance
 	s.window.SwapBuffers()
 	glfw.PollEvents()
 
 	// Get the image from the buffer.
-	return nil, nil
+	return rgba, nil
 }
 
 func (s *Slicer) prepareRender() error {
@@ -138,12 +153,18 @@ func (s *Slicer) prepareRender() error {
 
 	gl.UseProgram(s.program)
 
-	width, height := s.window.GetFramebufferSize()
-	projection := mgl32.Perspective(mgl32.DegToRad(45.0), float32(width)/float32(height), 0.1, 100.0)
-	projectionUniform := gl.GetUniformLocation(s.program, gl.Str("projection\x00"))
-	gl.UniformMatrix4fv(projectionUniform, 1, false, &projection[0])
+	// width, height := s.window.GetFramebufferSize()
+	// projection := mgl32.Perspective(mgl32.DegToRad(45.0), float32(width)/float32(height), 0.1, 100.0)
+	// projectionUniform := gl.GetUniformLocation(s.program, gl.Str("projection\x00"))
+	// gl.UniformMatrix4fv(projectionUniform, 1, false, &projection[0])
 
-	camera := mgl32.LookAtV(mgl32.Vec3{3, 3, 3}, mgl32.Vec3{0, 0, 0}, mgl32.Vec3{0, 1, 0})
+	// camera := mgl32.LookAtV(mgl32.Vec3{3, 3, 3}, mgl32.Vec3{0, 0, 0}, mgl32.Vec3{0, 1, 0})
+	left := float32(s.irmf.Min[0])
+	right := float32(s.irmf.Max[0])
+	bottom := float32(s.irmf.Min[1])
+	top := float32(s.irmf.Max[1])
+	near, far := float32(0.1), float32(100.0)
+	camera := mgl32.Ortho(left, right, bottom, top, near, far)
 	cameraUniform := gl.GetUniformLocation(s.program, gl.Str("camera\x00"))
 	gl.UniformMatrix4fv(cameraUniform, 1, false, &camera[0])
 
@@ -151,10 +172,12 @@ func (s *Slicer) prepareRender() error {
 	s.modelUniform = gl.GetUniformLocation(s.program, gl.Str("model\x00"))
 	gl.UniformMatrix4fv(s.modelUniform, 1, false, &s.model[0])
 
+	// Set up uniforms needed by shaders:
+
 	// textureUniform := gl.GetUniformLocation(s.program, gl.Str("tex\x00"))
 	// gl.Uniform1i(textureUniform, 0)
 
-	gl.BindFragDataLocation(s.program, 0, gl.Str("outputColor\x00"))
+	gl.BindFragDataLocation(s.program, 0, gl.Str("out_FragColor\x00"))
 
 	// // Load the texture
 	// texture, err := newTexture("square.png")
@@ -246,130 +269,11 @@ func compileShader(source string, shaderType uint32) (uint32, error) {
 }
 
 const vertexShader = `#version 300 es
-// from threejs:
-// #define varying in
-//
-// #define gl_FragDepthEXT gl_FragDepth
-// #define texture2D texture
-// #define textureCube texture
-// #define texture2DProj textureProj
-// #define texture2DLodEXT textureLod
-// #define texture2DProjLodEXT textureProjLod
-// #define textureCubeLodEXT textureLod
-// #define texture2DGradEXT textureGrad
-// #define texture2DProjGradEXT textureProjGrad
-// #define textureCubeGradEXT textureGrad
-// precision highp float;
-// precision highp int;
-// #define SHADER_NAME ShaderMaterial
-// #define GAMMA_FACTOR 2
-// #define DOUBLE_SIDED
-// uniform mat4 viewMatrix;
-// uniform vec3 cameraPosition;
-// #define TONE_MAPPING
-// #ifndef saturate
-// 	#define saturate(a) clamp( a, 0.0, 1.0 )
-// #endif
-// uniform float toneMappingExposure;
-// uniform float toneMappingWhitePoint;
-// vec3 LinearToneMapping( vec3 color ) {
-// 	return toneMappingExposure * color;
-// }
-// vec3 ReinhardToneMapping( vec3 color ) {
-// 	color *= toneMappingExposure;
-// 	return saturate( color / ( vec3( 1.0 ) + color ) );
-// }
-// #define Uncharted2Helper( x ) max( ( ( x * ( 0.15 * x + 0.10 * 0.50 ) + 0.20 * 0.02 ) / ( x * ( 0.15 * x + 0.50 ) + 0.20 * 0.30 ) ) - 0.02 / 0.30, vec3( 0.0 ) )
-// vec3 Uncharted2ToneMapping( vec3 color ) {
-// 	color *= toneMappingExposure;
-// 	return saturate( Uncharted2Helper( color ) / Uncharted2Helper( vec3( toneMappingWhitePoint ) ) );
-// }
-// vec3 OptimizedCineonToneMapping( vec3 color ) {
-// 	color *= toneMappingExposure;
-// 	color = max( vec3( 0.0 ), color - 0.004 );
-// 	return pow( ( color * ( 6.2 * color + 0.5 ) ) / ( color * ( 6.2 * color + 1.7 ) + 0.06 ), vec3( 2.2 ) );
-// }
-// vec3 ACESFilmicToneMapping( vec3 color ) {
-// 	color *= toneMappingExposure;
-// 	return saturate( ( color * ( 2.51 * color + 0.03 ) ) / ( color * ( 2.43 * color + 0.59 ) + 0.14 ) );
-// }
-// vec3 toneMapping( vec3 color ) { return LinearToneMapping( color ); }
-
-// vec4 LinearToLinear( in vec4 value ) {
-// 	return value;
-// }
-// vec4 GammaToLinear( in vec4 value, in float gammaFactor ) {
-// 	return vec4( pow( value.rgb, vec3( gammaFactor ) ), value.a );
-// }
-// vec4 LinearToGamma( in vec4 value, in float gammaFactor ) {
-// 	return vec4( pow( value.rgb, vec3( 1.0 / gammaFactor ) ), value.a );
-// }
-// vec4 sRGBToLinear( in vec4 value ) {
-// 	return vec4( mix( pow( value.rgb * 0.9478672986 + vec3( 0.0521327014 ), vec3( 2.4 ) ), value.rgb * 0.0773993808, vec3( lessThanEqual( value.rgb, vec3( 0.04045 ) ) ) ), value.a );
-// }
-// vec4 LinearTosRGB( in vec4 value ) {
-// 	return vec4( mix( pow( value.rgb, vec3( 0.41666 ) ) * 1.055 - vec3( 0.055 ), value.rgb * 12.92, vec3( lessThanEqual( value.rgb, vec3( 0.0031308 ) ) ) ), value.a );
-// }
-// vec4 RGBEToLinear( in vec4 value ) {
-// 	return vec4( value.rgb * exp2( value.a * 255.0 - 128.0 ), 1.0 );
-// }
-// vec4 LinearToRGBE( in vec4 value ) {
-// 	float maxComponent = max( max( value.r, value.g ), value.b );
-// 	float fExp = clamp( ceil( log2( maxComponent ) ), -128.0, 127.0 );
-// 	return vec4( value.rgb / exp2( fExp ), ( fExp + 128.0 ) / 255.0 );
-// }
-// vec4 RGBMToLinear( in vec4 value, in float maxRange ) {
-// 	return vec4( value.rgb * value.a * maxRange, 1.0 );
-// }
-// vec4 LinearToRGBM( in vec4 value, in float maxRange ) {
-// 	float maxRGB = max( value.r, max( value.g, value.b ) );
-// 	float M = clamp( maxRGB / maxRange, 0.0, 1.0 );
-// 	M = ceil( M * 255.0 ) / 255.0;
-// 	return vec4( value.rgb / ( M * maxRange ), M );
-// }
-// vec4 RGBDToLinear( in vec4 value, in float maxRange ) {
-// 	return vec4( value.rgb * ( ( maxRange / 255.0 ) / value.a ), 1.0 );
-// }
-// vec4 LinearToRGBD( in vec4 value, in float maxRange ) {
-// 	float maxRGB = max( value.r, max( value.g, value.b ) );
-// 	float D = max( maxRange / maxRGB, 1.0 );
-// 	D = min( floor( D ) / 255.0, 1.0 );
-// 	return vec4( value.rgb * ( D * ( 255.0 / maxRange ) ), D );
-// }
-// const mat3 cLogLuvM = mat3( 0.2209, 0.3390, 0.4184, 0.1138, 0.6780, 0.7319, 0.0102, 0.1130, 0.2969 );
-// vec4 LinearToLogLuv( in vec4 value )  {
-// 	vec3 Xp_Y_XYZp = cLogLuvM * value.rgb;
-// 	Xp_Y_XYZp = max( Xp_Y_XYZp, vec3( 1e-6, 1e-6, 1e-6 ) );
-// 	vec4 vResult;
-// 	vResult.xy = Xp_Y_XYZp.xy / Xp_Y_XYZp.z;
-// 	float Le = 2.0 * log2(Xp_Y_XYZp.y) + 127.0;
-// 	vResult.w = fract( Le );
-// 	vResult.z = ( Le - ( floor( vResult.w * 255.0 ) ) / 255.0 ) / 255.0;
-// 	return vResult;
-// }
-// const mat3 cLogLuvInverseM = mat3( 6.0014, -2.7008, -1.7996, -1.3320, 3.1029, -5.7721, 0.3008, -1.0882, 5.6268 );
-// vec4 LogLuvToLinear( in vec4 value ) {
-// 	float Le = value.z * 255.0 + value.w;
-// 	vec3 Xp_Y_XYZp;
-// 	Xp_Y_XYZp.y = exp2( ( Le - 127.0 ) / 2.0 );
-// 	Xp_Y_XYZp.z = Xp_Y_XYZp.y / value.y;
-// 	Xp_Y_XYZp.x = value.x * Xp_Y_XYZp.z;
-// 	vec3 vRGB = cLogLuvInverseM * Xp_Y_XYZp.rgb;
-// 	return vec4( max( vRGB, 0.0 ), 1.0 );
-// }
-// vec4 mapTexelToLinear( vec4 value ) { return LinearToLinear( value ); }
-// vec4 matcapTexelToLinear( vec4 value ) { return LinearToLinear( value ); }
-// vec4 envMapTexelToLinear( vec4 value ) { return LinearToLinear( value ); }
-// vec4 emissiveMapTexelToLinear( vec4 value ) { return LinearToLinear( value ); }
-// vec4 linearToOutputTexel( vec4 value ) { return LinearToLinear( value ); }
-// end from threejs.
-
-uniform vec3 u_ll;
-uniform vec3 u_ur;
 out vec4 v_xyz;
 void main() {
-  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-  v_xyz = modelMatrix * vec4( position, 1.0 );
+  // gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+	// v_xyz = modelMatrix * vec4( position, 1.0 );
+	v_xyz = gl_Position;
 }
 `
 
